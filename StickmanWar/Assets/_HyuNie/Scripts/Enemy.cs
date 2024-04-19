faf5c63f7e63e131014341adca671f19fea04937
Nguyễn Hữu Niên <125124829+NguyenHuuNien@users.noreply.github.com>
2024-04-19 14:19:43 +0700

    enemy move to player

--- StickmanWar/Assets/_HyuNie/Scripts/Enemy.cs
+++ StickmanWar/Assets/_HyuNie/Scripts/Enemy.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class Enemy : Character
 {
+    private GameObject player;
     private State stateEnemy;
     
     private void OnEnable() {
         changState(new StateMove(this));
     }
     private void Start() {
+        player = FindObjectOfType<Player>().gameObject;
+        GameController.Instance.SetDataCharacter(this);
         eCharacter = ECharacter.Enemy;
+        changState(new StateMove(this));
     }
     private void Update() {
         if(stateEnemy!=null) stateEnemy.OnExecute();
         if(Heart<=0) OnDead();
+        if(checkPlayer()) changState(new StateAttack(this));
+        else changState(new StateMove(this));
     }
     public void changState(State newState){
         stateEnemy?.OnExit();
@@ -22,7 +28,16 @@ public class Enemy : Character
         stateEnemy?.OnEnter();
     }
     public void MoveToPlayer(){
-
+        Vector3 target = new Vector3(player.transform.position.x, transform.position.y,0);
+        transform.position = Vector3.MoveTowards(transform.position, target,Time.deltaTime);
+    }
+    public void AttackPlayer(){
+        Debug.Log("Attack Player");
+    }
+    private bool checkPlayer(){
+        Vector2 direction = player.transform.position - transform.position;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, 1.2f,LayerMask.GetMask("Player"));
+        return hit.collider != null;
     }
     public void GetDame(float dame){
         Heart -= dame;
--- StickmanWar/Assets/_HyuNie/Scripts/StateAttack.cs
+++ StickmanWar/Assets/_HyuNie/Scripts/StateAttack.cs
@@ -1,6 +1,6 @@
 public class StateAttack : State{
     public StateAttack(Enemy enemy) : base(enemy) { }
     public override void OnExecute(){
-        
+        _enemy.AttackPlayer();
     }
 }
