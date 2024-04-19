7322ea1fb50eca97c0df3284d6605756ed982fc3
オアイン <134583482+vuoanh291104@users.noreply.github.com>
2024-04-19 13:26:24 +0700

    move

--- StickmanWar/Assets/_Oanh/Scripts/Player.cs
+++ StickmanWar/Assets/_Oanh/Scripts/Player.cs
@@ -3,20 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : Character
-{
-    [SerializeField] Transform target;
+{   
+    private Vector3 targetPosition;
+    // [SerializeField] Transform target;
     // Start is called before the first frame update
     void Start()
     {
         eCharacter = ECharacter.Player;
+        targetPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.MoveTowards(transform.position,targetPosition,Time.deltaTime*5f);
     }
-    public void move(){
-        Vector3 direction= Vector3.MoveTowards(transform.position,target.position,Time.deltaTime);
+    public void move(float x){
+        targetPosition = new Vector3(transform.position.x + x, transform.position.y , 0);
     }
 }
