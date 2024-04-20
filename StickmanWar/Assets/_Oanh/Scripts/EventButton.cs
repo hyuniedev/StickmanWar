6d3afed4b991d8d4d7599e816340828469083f26
オアイン <134583482+vuoanh291104@users.noreply.github.com>
2024-04-20 16:27:08 +0700

    fix_move

--- StickmanWar/Assets/_Oanh/Scripts/EventButton.cs
+++ StickmanWar/Assets/_Oanh/Scripts/EventButton.cs
@@ -2,10 +2,25 @@ using UnityEngine;
 
 public class EventButton: MonoBehaviour {
     [SerializeField] private Player player;
+    public Look lookRange;
     public void buttonMove_Left(){
-        player.move(-3f);
+        lookRange.transform.position = new Vector3(player.transform.position.x -3f, player.transform.position.y, player.transform.position.z);
+        if(Look.isLook){
+            player.moveToEnemy();
+            Look.isLook = false;
+        }else{
+            player.move(-2f);
+
+        }
     }
     public void buttonMove_Right(){
-        player.move(3f);
+        lookRange.transform.position = new Vector3(player.transform.position.x +3f, player.transform.position.y, player.transform.position.z);
+        if(Look.isLook){
+            player.moveToEnemy();
+            Look.isLook=false;
+        }else{
+            player.move(2f);
+        }   
+        
     }
 }
