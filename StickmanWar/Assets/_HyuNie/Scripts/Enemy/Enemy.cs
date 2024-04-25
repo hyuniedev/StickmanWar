8d275caa3ad21e63f572af1dd99a2901dbb05216
Nguyễn Hữu Niên <125124829+NguyenHuuNien@users.noreply.github.com>
2024-04-26 00:21:11 +0700

    anika kaerimasu

--- StickmanWar/Assets/_HyuNie/Scripts/Enemy/Enemy.cs
+++ StickmanWar/Assets/_HyuNie/Scripts/Enemy/Enemy.cs
@@ -6,45 +6,54 @@ public class Enemy : Character
 {
     private GameObject player;
     private State stateEnemy;
-    public EDirec eDirec{get; set;}
-    public bool okGetDame{get; set;}
-    private void OnEnable() {
+    public EDirec eDirec { get; set; }
+    public bool okGetDame { get; set; }
+    private void OnEnable()
+    {
         okGetDame = true;
         changState(new StateMove(this));
     }
-    private void Start() {
+    private void Start()
+    {
         player = FindObjectOfType<Player>().gameObject;
         GameController.Instance.SetDataCharacter(this);
         eCharacter = ECharacter.Enemy;
         changState(new StateMove(this));
         okGetDame = true;
     }
-    private void Update() {
-        if(stateEnemy!=null) stateEnemy.OnExecute();
-        if(Heart<=0) OnDead();
-        if(checkPlayer()) changState(new StateAttack(this));
+    private void Update()
+    {
+        if (stateEnemy != null) stateEnemy.OnExecute();
+        if (Heart <= 0) OnDead();
+        if (checkPlayer()) changState(new StateAttack(this));
         else changState(new StateMove(this));
     }
-    public void changState(State newState){
+    public void changState(State newState)
+    {
         stateEnemy?.OnExit();
         stateEnemy = newState;
         stateEnemy?.OnEnter();
     }
-    public void MoveToPlayer(){
-        Vector3 target = new Vector3(player.transform.position.x, transform.position.y,0);
-        transform.position = Vector3.MoveTowards(transform.position, target,Time.deltaTime);
+    public void MoveToPlayer()
+    {
+        Vector3 target = new Vector3(player.transform.position.x, transform.position.y, 0);
+        transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime);
     }
-    public void AttackPlayer(){
+    public void AttackPlayer()
+    {
         Debug.Log("Attack Player");
     }
-    private bool checkPlayer(){
+    private bool checkPlayer()
+    {
         Collider2D check = Physics2D.OverlapCircle(transform.position, 1, LayerMask.GetMask("Player"));
         return check != null;
     }
-    public void GetDame(float dame){
+    public void GetDame(float dame)
+    {
         Heart -= dame;
     }
-    private void OnDead(){
+    private void OnDead()
+    {
         QueueEnemy.Instance.addEnemy(this);
     }
 }
--- StickmanWar/Assets/_HyuNie/Scripts/PlayerAuto/PlayerAuto.cs
+++ StickmanWar/Assets/_HyuNie/Scripts/PlayerAuto/PlayerAuto.cs
@@ -1,22 +1,31 @@
 using UnityEngine;
 
-public class PlayerAuto : Character{
+public class PlayerAuto : Character
+{
     private IState state;
-    public GameObject enemy{get; set;}
-    private void Update() {
+    public GameObject enemy { get; set; }
+    private void Update()
+    {
+        if (enemy)
+            Debug.DrawLine(transform.position, enemy.transform.position, Color.gray);
         state?.OnExecute(this);
-        if(enemy==null){
+        if (enemy == null)
+        {
             changeState(new PLStateIdle());
-        }else{
+        }
+        else
+        {
             changeState(new PLStateAttack());
         }
     }
-    public void changeState(IState newState){
+    public void changeState(IState newState)
+    {
         state?.OnExit(this);
         state = newState;
         state?.OnEnter(this);
     }
-    public GameObject findEnemy(){
+    public GameObject findEnemy()
+    {
         return EnemySystem.Instance.getTargetEnemy();
     }
 }
