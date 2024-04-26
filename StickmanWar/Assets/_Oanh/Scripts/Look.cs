77871f503bed370aeab475a6fdbd20fcd14f8ed0
Nguyễn Hữu Niên <125124829+NguyenHuuNien@users.noreply.github.com>
2024-04-26 17:36:39 +0700

    off Look of Oanh bao

--- StickmanWar/Assets/_Oanh/Scripts/Look.cs
+++ StickmanWar/Assets/_Oanh/Scripts/Look.cs
@@ -5,29 +5,32 @@ using UnityEngine;
 
 public class Look : MonoBehaviour
 {
-    
+
     public static Look instance;
-    public static bool isLook=false;
+    public static bool isLook = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
     }
-    private void OnTriggerEnter2D(Collider2D other) {
-        Debug.Log(other.gameObject.tag);
-        if(other.gameObject.tag.Equals("Enemy")){
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag.Equals("Enemy"))
+        {
             isLook = true;
             // Debug.Log("mi");
         }
     }
-    private void OnCollisionEnter2D(Collision2D other) {
+    private void OnCollisionEnter2D(Collision2D other)
+    {
         Debug.Log("Co va cham");
-        if(other.gameObject.tag.Equals("Enemy")){
+        if (other.gameObject.tag.Equals("Enemy"))
+        {
             Debug.Log("Collider look with Enemy");
         }
     }
