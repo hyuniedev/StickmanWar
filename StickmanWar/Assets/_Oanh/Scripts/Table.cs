e5b411e540aa779b3685f556d6582679346099e1
オアイン <134583482+vuoanh291104@users.noreply.github.com>
2024-04-27 21:31:52 +0700

    Table_donee

--- StickmanWar/Assets/_Oanh/Scripts/Table.cs
+++ StickmanWar/Assets/_Oanh/Scripts/Table.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Table : MonoBehaviour
 {
+    [SerializeField] private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.SetActive(false);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void OnTable(){
+        if(anim.GetInteger("IsOn")==0){
+            anim.SetInteger("IsOn",1);
+        }else if(anim.GetInteger("IsOn")==1){
+            anim.SetInteger("IsOn",2);
+        }else if(anim.GetInteger("IsOn")==2){
+            anim.SetInteger("IsOn",3);
+        }else{
+            anim.SetInteger("IsOn",2);
+        }
     }
 }
